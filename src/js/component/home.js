fd3344755721fea58b030a85d9048eb7efa1b29d
nitlara <nitlara@gmail.com>
2021-01-08 18:50:51 +0000

    import jumbotron into home

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,14 +4,17 @@ import { footer } from "./footer.js";
 import { jumbotron } from "./jumbotron.js";
 import { navbar } from "./navbar.js";
 
+//import boostrap elements
+import Jumbotron from "react-bootstrap/Jumbotron";
+
 //create your first component
 export function Home() {
 	return (
 		<div className="container">
-            <navbar />
-            <jumbotron />
-            <cards />
-            <footer />
-        </div>
+			<navbar />
+			<jumbotron />
+			<cards />
+			<footer />
+		</div>
 	);
 }
---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+
+import "bootstrap";
+
+export function jumbotron() {
+	return (
+		<div>
+			<Jumbotron fluid>
+				<Container>
+					<h1>A warm wellcome</h1>
+					<p>
+						This is a modified jumbotron that occupies the entire
+						horizontal space of its parent.
+					</p>
+				</Container>
+			</Jumbotron>
+		</div>
+	);
+}
