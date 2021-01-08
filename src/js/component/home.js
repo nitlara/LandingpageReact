603108cde3f975d0520469f09211b91382df094f
nitlara <nitlara@gmail.com>
2021-01-08 19:19:31 +0000

    deleted elements in home

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,20 +1,14 @@
 import React from "react";
 import { Cards } from "./cards.js";
 import { Footer } from "./footer.js";
-import { JumbotronComponent } from "./jumbotron.js";
 import { Navbar } from "./navbar.js";
-
-//import boostrap elements
-import Jumbotron from "react-bootstrap/Jumbotron";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
 
 //create your first component
 export function Home() {
 	return (
 		<div className="container">
 			<Navbar />
-			<Jumbotron />
+			<JumbotronComponent />
 			<Cards />
 			<Footer />
 		</div>
