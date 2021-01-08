ab0b009479863ffa1f0f2ee82b53feba8473152c
nitlara <nitlara@gmail.com>
2021-01-08 19:14:22 +0000

    added some more elements bootstrap

--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+import Container from 'react-bootstrap/Container';
 
 import "bootstrap";
 
@@ -11,8 +13,12 @@ export function Jumbotron() {
 					<h1>A warm wellcome</h1>
 					<p>
 						This is a modified jumbotron that occupies the entire
+						horizontal space of its parent. This is a modified jumbotron that occupies the entire
 						horizontal space of its parent.
 					</p>
+                   <>
+                    <Button variant="primary">Primary</Button>{' '}
+                   </>
 				</Container>
 			</Jumbotron>
 		</div>
