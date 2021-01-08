5b67014cdcaa3025fe2dbcd2ea1c2dc6dfdcc513
nitlara <nitlara@gmail.com>
2021-01-08 21:14:07 +0000

    cards:4 cards

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -10,7 +10,7 @@ export function Cards() {
 	return (
 		<CardGroup>
 			<Card>
-				<Card.Img variant="top" src="holder.js/100px160" />
+				<Card.Img variant="top" src="holder.js/500x325" />
 				<Card.Body>
 					<Card.Title>Card title</Card.Title>
 					<Card.Text>
@@ -22,7 +22,7 @@ export function Cards() {
 				</Card.Body>
 			</Card>
 			<Card>
-				<Card.Img variant="top" src="holder.js/100px160" />
+				<Card.Img variant="top" src="holder.js/500x325" />
 				<Card.Body>
 					<Card.Title>Card title</Card.Title>
 					<Card.Text>
@@ -34,7 +34,19 @@ export function Cards() {
 				</Card.Body>
 			</Card>
 			<Card>
-				<Card.Img variant="top" src="holder.js/100px160" />
+				<Card.Img variant="top" src="holder.js/500x325" />
+				<Card.Body>
+					<Card.Title>Card title</Card.Title>
+					<Card.Text>
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</Card.Text>
+					<Button variant="primary">Go somewhere</Button>
+				</Card.Body>
+			</Card>
+			<Card>
+				<Card.Img variant="top" src="holder.js/500x325" />
 				<Card.Body>
 					<Card.Title>Card title</Card.Title>
 					<Card.Text>
