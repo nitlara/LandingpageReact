4cd30693954bebe45987c0ef18f6af49b9d6aaba
nitlara <nitlara@gmail.com>
2021-01-08 19:16:15 +0000

    change names components

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Cards } from "./cards.js";
 import { Footer } from "./footer.js";
-import { Jumbotron } from "./jumbotron.js";
+import { JumbotronComponent } from "./jumbotron.js";
 import { Navbar } from "./navbar.js";
 
 //import boostrap elements
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 //create your first component
 export function Home() {
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
-import Container from 'react-bootstrap/Container';
+import Container from "react-bootstrap/Container";
 
 import "bootstrap";
 
-export function Jumbotron() {
+export function JumbotronComponent() {
 	return (
 		<div>
 			<Jumbotron fluid>
@@ -13,12 +13,13 @@ export function Jumbotron() {
 					<h1>A warm wellcome</h1>
 					<p>
 						This is a modified jumbotron that occupies the entire
-						horizontal space of its parent. This is a modified jumbotron that occupies the entire
-						horizontal space of its parent.
+						horizontal space of its parent. This is a modified
+						jumbotron that occupies the entire horizontal space of
+						its parent.
 					</p>
-                   <>
-                    <Button variant="primary">Primary</Button>{' '}
-                   </>
+					<>
+						<Button variant="primary">Primary</Button>{" "}
+					</>
 				</Container>
 			</Jumbotron>
 		</div>
