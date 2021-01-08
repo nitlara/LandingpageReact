4dfb1fff23313938867af976495f3ed0413f4b32
nitlara <nitlara@gmail.com>
2021-01-08 18:40:09 +0000

    home: functions and components introduced

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,25 +3,15 @@ import { cards } from "./cards.js";
 import { footer } from "./footer.js";
 import { jumbotron } from "./jumbotron.js";
 import { navbar } from "./navbar.js";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+            <navbar />
+            <jumbotron />
+            <cards />
+            <footer />
+        </div>
 	);
 }
