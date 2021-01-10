b4ff684ac15fad6b1bd88b249e2de5bc5b4bf6d8
Alvarodevs <alvaro.develops@gmail.com>
2021-01-10 22:41:50 +0000

    Navbar collapse position updated, Navbar finished

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
+import {
+	Navbar,
+	Nav,
+	NavItem,
+	NavDropdown,
+	MenuItem,
+	Col
+} from "react-bootstrap";
 
 //CON ESTOS IMPORTS TAMPOCO RECONOCE EL MODULE
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -15,24 +22,19 @@ export function Navbar2() {
 
 	return (
 		<div>
-			<Navbar
-				collapseOnSelect
-				expand="lg"
-				bg="dark"
-				variant="dark"
-				className="d-flex justify-content-between">
+			<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 				<Navbar.Brand href="#home">Start Bootstrap</Navbar.Brand>
-				<div>
-					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-					<Navbar.Collapse id="responsive-navbar-nav">
-						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#about">About</Nav.Link>
-							<Nav.Link href="#services">Services</Nav.Link>
-							<Nav.Link href="#contact">Contact</Nav.Link>
-						</Nav>
-					</Navbar.Collapse>
-				</div>
+
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="ml-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#about">About</Nav.Link>
+						<Nav.Link href="#services">Services</Nav.Link>
+						<Nav.Link href="#contact">Contact</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Navbar>
 		</div>
 	);
