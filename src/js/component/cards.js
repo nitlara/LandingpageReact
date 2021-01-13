1475d56ab75ab2a74a6f29535e4e911d73df8d3a
nitlara <nitlara@gmail.com>
2021-01-13 10:49:46 +0000

    I am working on creating the cards from prop. I generate a new branch and when they work correctly I will make the pull request.

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -7,59 +7,14 @@ import CardGroup from "react-bootstrap/CardGroup";
 import "bootstrap";
 import { CardDeck } from "react-bootstrap";
 
-export function Cards() {
+export function Cards(props) {
 	return (
-		<CardDeck className="mb-4">
-			<Card className="border rounded text-center">
-				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
+		    <Card className="border rounded text-center">
+				<Card.Img variant="top" src={props.image} />
 				<Card.Body className="p-0">
-					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
+					<Card.Title className="mx-3 mt-4"> {props.title}</Card.Title>
 					<Card.Text className="mx-3">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
-					</Card.Text>
-					<div className="card-footer bg-transparent">
-						<Button variant="primary">Find Out More!</Button>
-					</div>
-				</Card.Body>
-			</Card>
-			<Card className="border rounded text-center">
-				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
-				<Card.Body className="p-0">
-					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
-					<Card.Text className="mx-3">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
-					</Card.Text>
-					<div className="card-footer bg-transparent">
-						<Button variant="primary">Find Out More!</Button>
-					</div>
-				</Card.Body>
-			</Card>
-			<Card className="border rounded text-center">
-				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
-				<Card.Body className="p-0">
-					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
-					<Card.Text className="mx-3">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
-					</Card.Text>
-					<div className="card-footer bg-transparent ">
-						<Button variant="primary">Find Out More!</Button>
-					</div>
-				</Card.Body>
-			</Card>
-			<Card className="border rounded text-center">
-				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
-				<Card.Body className="p-0">
-					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
-					<Card.Text className="mx-3">
-						This is a wider card with supporting text below as a
-						natural lead-in to additional content. This content is a
-						little bit longer.
+						{props.text}
 					</Card.Text>
 					<div className="card-footer bg-transparent">
 						<Button variant="primary">Find Out More!</Button>
@@ -69,3 +24,21 @@ export function Cards() {
 		</CardDeck>
 	);
 }
+
+Cards.propTypes = {
+    image: PropTypes.img,
+    title: PropTypes.string,
+    text: PropTypes.string,
+};
+
+
+
+render(){
+    return (
+    <CardDeck
+        image="http://placehold.it/500x325.jpg"
+        title= "Card title"
+        text= "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer"
+    />
+    )}
+    
---
+++ src/js/component/cards_copy.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import CardGroup from "react-bootstrap/CardGroup";
+
+import "bootstrap";
+import { CardDeck } from "react-bootstrap";
+
+export function Cards() {
+	return (
+		<CardDeck className="mb-4">
+			<Card className="border rounded text-center">
+				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
+				<Card.Body className="p-0">
+					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
+					<Card.Text className="mx-3">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</Card.Text>
+					<div className="card-footer bg-transparent">
+						<Button variant="primary">Find Out More!</Button>
+					</div>
+				</Card.Body>
+			</Card>
+			<Card className="border rounded text-center">
+				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
+				<Card.Body className="p-0">
+					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
+					<Card.Text className="mx-3">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</Card.Text>
+					<div className="card-footer bg-transparent">
+						<Button variant="primary">Find Out More!</Button>
+					</div>
+				</Card.Body>
+			</Card>
+			<Card className="border rounded text-center">
+				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
+				<Card.Body className="p-0">
+					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
+					<Card.Text className="mx-3">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</Card.Text>
+					<div className="card-footer bg-transparent ">
+						<Button variant="primary">Find Out More!</Button>
+					</div>
+				</Card.Body>
+			</Card>
+			<Card className="border rounded text-center">
+				<Card.Img variant="top" src="http://placehold.it/500x325.jpg" />
+				<Card.Body className="p-0">
+					<Card.Title className="mx-3 mt-4"> Card title</Card.Title>
+					<Card.Text className="mx-3">
+						This is a wider card with supporting text below as a
+						natural lead-in to additional content. This content is a
+						little bit longer.
+					</Card.Text>
+					<div className="card-footer bg-transparent">
+						<Button variant="primary">Find Out More!</Button>
+					</div>
+				</Card.Body>
+			</Card>
+		</CardDeck>
+	);
+}
