7d177e9c564833302d580038a4c829f429ffbb37
nitlara <nitlara@gmail.com>
2021-01-14 15:05:31 +0000

    cards added by map(element,index)

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,43 +7,50 @@ import { JumbotronComponent } from "./jumbotron.js";
 import { CardDeck } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import "bootstrap";
+import { element } from "prop-types";
 
 //create your first component
 export function Home() {
+	const allCards = [
+		{
+			title: "Card title 1",
+			image: "http://placehold.it/500x325.jpg",
+			text:
+				"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		},
+		{
+			title: "Card title 2",
+			image: "http://placehold.it/500x325.jpg",
+			text:
+				"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		},
+		{
+			title: "Card title 3",
+			image: "http://placehold.it/500x325.jpg",
+			text:
+				"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		},
+		{
+			title: "Card title 4",
+			image: "http://placehold.it/500x325.jpg",
+			text:
+				"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+		}
+	];
+
+	const allCardElements = allCards.map((element, index) => (
+		<Cards
+			key={index}
+			title={element.title}
+			image={element.image}
+			text={element.text}
+		/>
+	));
 	return (
 		<div className="container">
 			<Navbar2 />
 			<JumbotronComponent />
-			<CardDeck className="mb-4">
-				<Cards
-					title={"Card title"}
-					image={"http://placehold.it/500x325.jpg"}
-					text={
-						"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-					}
-				/>
-				<Cards
-					title={"Card title 2"}
-					image={"http://placehold.it/500x325.jpg"}
-					text={
-						"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-					}
-				/>
-				<Cards
-					title={"Card title 3"}
-					image={"http://placehold.it/500x325.jpg"}
-					text={
-						"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-					}
-				/>
-				<Cards
-					title={"Card title 4"}
-					image={"http://placehold.it/500x325.jpg"}
-					text={
-						"This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
-					}
-				/>
-			</CardDeck>
+			<CardDeck className="mb-4">{allCardElements}</CardDeck>
 			<Footer />
 		</div>
 	);
